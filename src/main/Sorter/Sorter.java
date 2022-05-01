f74775a4b4f15ee6eb78a10f75922eaaba1504cb
pLm-k <64797647+pLm-k@users.noreply.github.com>
2022-05-01 15:34:08 +0200

    added quicksort

---
+++ src/main/Sorter/Sorter.java
@@ -0,0 +1,98 @@
+package main.Sorter;
+
+import java.util.List;
+
+public class Sorter {
+
+	/**
+	 * selection sort
+	 * 
+	 * @param <T>  type of elements in list
+	 * @param list to be sorted
+	 */
+	public static <T extends Comparable<T>> void selectionSort(List<T> list) {
+		int max;
+		for(int i = 0; i < list.size() - 1; i++) {
+			max = i;
+			for(int j = i + 1; j < list.size(); j++) 
+				if(list.get(j).compareTo(list.get(max))>0) 
+					max = j;
+			swapElements(list, i, max);
+		}
+		
+	}
+
+	/**
+	 * insertion sort
+	 * 
+	 * @param <T>  type of elements in list
+	 * @param list to be sorted
+	 */
+	public static <T extends Comparable<T>> void insertionSort(List<T> list) {
+		for(int i = 1; i < list.size(); i++)
+			for(int j = i; j > 0; j--) 
+				if(list.get(j).compareTo(list.get(j-1))>0) 
+					swapElements(list, j, j-1);
+				else 
+					break;
+	}
+
+	/**
+	 * bubble sort
+	 * 
+	 * @param <T>  type of elements in list
+	 * @param list to be sorted
+	 */
+	public static <T extends Comparable<T>> void bubbleSort(List<T> list) {
+		for(int i = 0; i < list.size() - 1; i++) 
+			for(int j = 0; j < list.size() - 1 - i; j++) 
+				if(list.get(j).compareTo(list.get(j+1))<0)
+					swapElements(list, j, j+1);
+	}
+	
+	/**
+	 * quicksort
+	 * 
+	 * @param <T> type of elements in list
+	 * @param list to be sorted
+	 */
+	public static <T extends Comparable<T>> void quickSort(List<T> list) {
+		quickSort(list,0,list.size()-1);
+	}
+	
+	/**
+	 * @param <T> type of elements in list
+	 * @param list to be sorted
+	 * @param lowerBound of list part
+	 * @param upperBound of list part
+	 */
+	private static <T extends Comparable<T>> void quickSort(List<T> list, int lowerBound, int upperBound) {
+		if(upperBound <= lowerBound) {
+			return;
+		}
+		swapElements(list, (upperBound+lowerBound)/2, upperBound);
+		int firstElement = lowerBound;
+		for(int i = lowerBound; i <= upperBound; i++) {
+			if(list.get(i).compareTo(list.get(upperBound)) > 0) {
+				swapElements(list, i, firstElement);
+				firstElement++;
+			}
+		}
+		swapElements(list, firstElement, upperBound);
+		quickSort(list,lowerBound,firstElement-1);
+		quickSort(list,firstElement+1,upperBound);
+	}
+	
+	/**
+	 * @param <T> type of elements in list
+	 * @param list list to swap elements in
+	 * @param firstElement to swap
+	 * @param secondElement to swap
+	 */
+	private static <T extends Comparable<T>> void swapElements(List<T> list, int firstElement, int secondElement) {
+		T tmpElement = list.get(secondElement);
+		list.set(secondElement, list.get(firstElement));
+		list.set(firstElement, tmpElement);
+		
+	}
+}
---
+++ src/test/sorter/SorterTest.java
@@ -0,0 +1,76 @@
+package test.sorter;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import main.Sorter.Sorter;
+
+import java.util.LinkedList;
+
+public class SorterTest {
+	/**
+	 * some simple test for List
+	 * 
+	 * NOTE: These tests are in no way complete!
+	 * 
+	 * @author plmil
+	 *
+	 */
+	public class ListTest{
+		LinkedList<Integer> testList;
+		LinkedList<Integer> sortedTestList;
+		
+		@BeforeEach
+		public void setupList() {
+			testList = new LinkedList<Integer>();
+			testList.add(12);
+			testList.add(1);
+			testList.add(8);
+			testList.add(122);
+			testList.add(14);
+			testList.add(352);
+			testList.add(19);
+			testList.add(1);
+			testList.add(77);
+			testList.add(-123);
+			testList.add(0);
+			
+			LinkedList<Integer> sortedTestList = new LinkedList<Integer>();
+			sortedTestList.addAll(testList);
+			sortedTestList.sort((i1,i2) -> {return i2-i1;});
+		}
+		
+		@Test
+		public void bubbleSortTest() {
+			Sorter.bubbleSort(testList);
+			for(int i = 0; i <testList.size(); i++) {
+				Assertions.assertEquals(sortedTestList.get(i), testList.get(i));
+			}
+		}
+		
+		@Test
+		public void insertionSortTest() {
+			Sorter.insertionSort(testList);
+			for(int i = 0; i <testList.size(); i++) {
+				Assertions.assertEquals(sortedTestList.get(i), testList.get(i));
+			}
+		}
+		
+		@Test
+		public void selectionSortTest() {
+			Sorter.selectionSort(testList);
+			for(int i = 0; i <testList.size(); i++) {
+				Assertions.assertEquals(sortedTestList.get(i), testList.get(i));
+			}
+		}
+		
+		@Test
+		public void quickSortTest() {
+			Sorter.quickSort(testList);
+			for(int i = 0; i <testList.size(); i++) {
+				Assertions.assertEquals(sortedTestList.get(i), testList.get(i));
+			}
+		}
+	}
+}
